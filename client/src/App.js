8f5fba1c0766895828584cf21174472e6ec18d67
Saroopashree Kumaraguru <saroopa25@gmail.com>
2022-03-12 15:42:40 +0530

    wip: React app use REST APIs

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,73 @@
 import { useState, useEffect } from "react";
-import axios from "axios"
+import axios from "axios";
 
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Todo from "./components/todo";
+import { Button, Form, ListGroup } from "react-bootstrap";
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("second");
+
+  const fetchAllTodos = () => {
+    axios.get("server:8000/todo/").then((response) => {
+      setTodos(response.data);
+    });
+  };
+
+  const handleAddNewTodo = () => {
+    axios.post("server:8000/todo/", { desc: newTodo }).then((response) => {
+      let _todos = [...todos];
+      _todos.push(response.data);
+      setTodos(_todos);
+      setNewTodo("");
+    });
+  };
+
+  const handleTodoEdit = (id, desc, callback) => {
+    axios
+      .put(`server:8000/todo/change-desc/${id}`, { desc })
+      .then((response) => {
+        let _todos = [...todos];
+        _todos = _todos.map((t) => (t.id === id ? response.data : t));
+        setTodos(_todos);
+      })
+      .finally(callback);
+  };
+
+  const handleTodoToggle = (id) => {
+    axios.put(`server:8000/todo/toggle-complete/${id}`).then((response) => {
+      let _todos = [...todos];
+      _todos = _todos.map((t) => (t.id === id ? response.data : t));
+      setTodos(_todos);
+    });
+  };
+
+  const handleTodoDelete = (id) => {
+    axios.delete(`server:8000/todo/${id}`).then(fetchAllTodos);
+  };
 
   useEffect(() => {
-    axios.get("server:8000/todo/").then(response => {
-      setTodos(response.data);
-    })
-  }, [])
+    fetchAllTodos();
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app-start">
+      <h2 id="app-header">Todo App</h2>
+      <ListGroup>
+        {todos.map((todo) => (
+          <Todo
+            todo={todo}
+            handleTodoEdit={handleTodoEdit}
+            handleTodoToggle={handleTodoToggle}
+            handleTodoDelete={handleTodoDelete}
+          />
+        ))}
+      </ListGroup>
+      <Form.Group onSubmit={handleAddNewTodo}>
+        <Form.Control plaintext value={newTodo} onChange={setNewTodo} />
+        <Button variant="success">Add Todo #{todos.length}</Button>
+      </Form.Group>
     </div>
   );
 }
--- client/src/components/todo.js
+++ client/src/components/todo.js
@@ -1,32 +1,65 @@
 import { useState } from "react";
-import { Card, Form } from "react-bootstrap";
+import { PropTypes } from "prop-types";
+import { Button, Form, ListGroup } from "react-bootstrap";
 
-function Todo(props) {
-  const [todoDesc, setTodoDesc] = useState(props.desc)
+import "./todo.module.css";
+
+const Todo = (props) => {
+  const [todoDesc, setTodoDesc] = useState(props.todo.desc);
   const [editMode, setEditMode] = useState(false);
 
   return (
-    <Card bg="dark">
-      <Card.Body>
+    <ListGroup.Item bg="dark">
+      <div>
         {editMode ? (
           <Form>
-              <Form.Group>
-                  <Form.Control plaintext value={todoDesc} onChange={setTodoDesc} />
-              </Form.Group>
+            <Form.Group
+              onSubmit={() =>
+                props.handleTodoEdit(props.todo.id, todoDesc, () =>
+                  setEditMode(false)
+                )
+              }
+            >
+              <Form.Control plaintext value={todoDesc} onChange={setTodoDesc} />
+            </Form.Group>
           </Form>
         ) : (
-          <Card.Text
-            style={
-              props.is_completed
-                ? { textDecoration: "line-through", color: "#AAA" }
-                : {}
-            }
-            onClick={() => setEditMode(!editMode)}
-          >
-            {props.desc}
-          </Card.Text>
+          <div className="todo-item">
+            <div className="checkbox-desc-wrapper">
+              <input
+                type="checkbox"
+                onClick={() => props.handleTodoToggle(props.todo.id)}
+              />
+              <div
+                style={
+                  props.is_completed
+                    ? { textDecoration: "line-through", color: "#AAA" }
+                    : {}
+                }
+                onClick={() => setEditMode(!editMode)}
+              >
+                {props.desc}
+              </div>
+            </div>
+            <Button variant="danger" onClick={() => props.handleTodoDelete(props.todo.id)}>
+              X
+            </Button>
+          </div>
         )}
-      </Card.Body>
-    </Card>
+      </div>
+    </ListGroup.Item>
   );
-}
+};
+
+Todo.propTypes = {
+  todo: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    desc: PropTypes.string.isRequired,
+    is_completed: PropTypes.bool.isRequired,
+  }),
+  handleTodoEdit: PropTypes.func.isRequired,
+  handleTodoToggle: PropTypes.func.isRequired,
+  handleTodoDelete: PropTypes.func.isRequired,
+};
+
+export default Todo;
